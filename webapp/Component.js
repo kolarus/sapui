8dac6756467a136f79c8b8fadb77ff4f9f53d60c
Bohdan Morozov <road2ps@gmail.com>
2019-04-10 09:24:54 +0000

    initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,38 @@
+sap.ui.define([
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+   "use strict";
+   return UIComponent.extend("ToDo.Component", {
+      metadata : {
+      	manifest: "json"
+      },
+      
+      init : function () {
+         // call the init function of the parent
+         UIComponent.prototype.init.apply(this, arguments);
+         // set data model
+         this.setModel(new JSONModel({
+					features: [
+						"Enterprise-Ready Web Toolkit",
+						"Powerful Development Concepts",
+						"Feature-Rich UI Controls",
+						"Consistent User Experience",
+						"Free and Open Source",
+						"Responsive Across Browsers and Devices"
+					],
+					toDoInput: "Walk the dog",
+					toDo: [
+						"Learn sapui5"
+					]
+				})
+			);
+			
+		var i18nModel = new ResourceModel({
+        bundleName: "ToDo.i18n.i18n"
+         });
+         this.setModel(i18nModel, "i18n");
+      }
+   });
+});
